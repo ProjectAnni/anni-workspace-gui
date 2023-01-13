3dc496249d3ebea3f0f9d47ad47d7a9bf2bf6d32
Eridanus Sora <sora@sound.moe>
2023-01-13 22:44:34 +0800

    edit title & catalog & release date

--- src/components/Workspace/MainEditor/AlbumEditor/AlbumMetaInfoEditor/index.tsx
+++ src/components/Workspace/MainEditor/AlbumEditor/AlbumMetaInfoEditor/index.tsx
@@ -3,16 +3,38 @@ import { useAtom } from "jotai";
 import { Button, FormGroup, Icon, InputGroup } from "@blueprintjs/core";
 import { AlbumDataActionTypes, AlbumDataReducerAtom } from "../state";
 import styles from "./index.module.scss";
+import EditReleaseDate from "./EditReleaseDate";
 
 interface Props {}
 
 const AlbumMetaInfoEditor: React.FC = () => {
     const [albumData, dispatch] = useAtom(AlbumDataReducerAtom);
-    const { album_id: albumId } = albumData || {};
+    const { album_id: albumId, title, catalog } = albumData || {};
 
     const onRefreshAlbumId = () => {
         dispatch({
-            type: AlbumDataActionTypes.GENERATE_NEW_ALBUM_ID,
+            type: AlbumDataActionTypes.GENERATE_NEW_ID,
+        });
+    };
+
+    const onTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: AlbumDataActionTypes.UPDATE_TITLE,
+            payload: e.target.value,
+        });
+    };
+
+    const onCatalogChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: AlbumDataActionTypes.UPDATE_CATALOG,
+            payload: e.target.value,
+        });
+    };
+
+    const onReleaseDateChange = (value: string) => {
+        dispatch({
+            type: AlbumDataActionTypes.UPDATE_RELEASE_DATE,
+            payload: value,
         });
     };
 
@@ -28,6 +50,13 @@ const AlbumMetaInfoEditor: React.FC = () => {
                     <Button icon="refresh" small onClick={onRefreshAlbumId} />
                 </div>
             </FormGroup>
+            <FormGroup label="标题" labelInfo="(required)">
+                <InputGroup value={title} onChange={onTitleChange} />
+            </FormGroup>
+            <FormGroup label="品番" labelInfo="(required)">
+                <InputGroup value={catalog} onChange={onCatalogChange} />
+            </FormGroup>
+            <EditReleaseDate onChange={onReleaseDateChange} />
         </>
     );
 };
--- src/components/Workspace/MainEditor/AlbumEditor/state.ts
+++ src/components/Workspace/MainEditor/AlbumEditor/state.ts
@@ -5,7 +5,13 @@ export enum AlbumDataActionTypes {
     /** 重置数据 */
     RESET,
     /** 随机一个新的ALBUM ID */
-    GENERATE_NEW_ALBUM_ID,
+    GENERATE_NEW_ID,
+    /** 设置标题 */
+    UPDATE_TITLE,
+    /** 设置品番 */
+    UPDATE_CATALOG,
+    /** 设置发售日期 */
+    UPDATE_RELEASE_DATE,
 }
 
 type AlbumDataActionPayload =
@@ -14,7 +20,19 @@ type AlbumDataActionPayload =
           payload: AlbumData;
       }
     | {
-          type: AlbumDataActionTypes.GENERATE_NEW_ALBUM_ID;
+          type: AlbumDataActionTypes.GENERATE_NEW_ID;
+      }
+    | {
+          type: AlbumDataActionTypes.UPDATE_TITLE;
+          payload: string;
+      }
+    | {
+          type: AlbumDataActionTypes.UPDATE_CATALOG;
+          payload: string;
+      }
+    | {
+          type: AlbumDataActionTypes.UPDATE_RELEASE_DATE;
+          payload: string;
       };
 
 const albumDataReducer = (
@@ -24,10 +42,28 @@ const albumDataReducer = (
     if (action.type === AlbumDataActionTypes.RESET) {
         return action.payload;
     }
-    if (action.type === AlbumDataActionTypes.GENERATE_NEW_ALBUM_ID) {
+    if (action.type === AlbumDataActionTypes.GENERATE_NEW_ID) {
         return {
             ...prev!,
             album_id: window.crypto.randomUUID(),
+        };
+    }
+    if (action.type === AlbumDataActionTypes.UPDATE_TITLE) {
+        return {
+            ...prev!,
+            title: action.payload,
+        };
+    }
+    if (action.type === AlbumDataActionTypes.UPDATE_CATALOG) {
+        return {
+            ...prev!,
+            catalog: action.payload,
+        };
+    }
+    if (action.type === AlbumDataActionTypes.UPDATE_RELEASE_DATE) {
+        return {
+            ...prev!,
+            date: action.payload,
         };
     }
     throw new Error("unknown action type");
